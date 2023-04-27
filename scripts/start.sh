84207adbba794c4769ec72c3f2d60f04b9989364
alextjj <254334386@qq.com>
2023-04-27 11:45:13 +0800

    up  client define test  model

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,10 +5,10 @@ set -o errexit
 # 1. 正常模式      normal
 # 2. 网路链接模式   connect
 # 3. ping网关模式  ping 
-export TEST_MODE=ping
+export TEST_MODE=normal
 
 # 间隔多少MS 添加一个客户端
-export ADD_CLIENT_CD_MS=100
+export ADD_CLIENT_CD_MS=200
 
 
 export AGENT_URL=agent601-dev.game.melandworld.com
@@ -16,7 +16,7 @@ export AGENT_URL=agent601-dev.game.melandworld.com
 # export AGENT_URL=192.168.50.15:5700
 
 # 压测的客户端数量上限
-export CLIENT_NUM=2000
+export CLIENT_NUM=500
 
 # 压测的客户端模拟 Id 起始值 
 export CLIENT_IDX_BEGIN=10000
